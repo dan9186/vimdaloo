f6eda3275ee4260451242f0440d7251d9f070be3
Daniel Hess <dan9186@gmail.com>
2015-03-30 11:48:07 -0700

    adding some better install checks

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-$HOME/.vim/bundle/YouCompleteMe/install.sh
+# Singular deps check, may want foreach later
+DEPS=$(ls "$PWD/bundle" | head -n 1)
+if [[ ! -f "$PWD/bundle/$DEPS/.git" ]]; then
+	echo "Dependencies don't exist; you probably forgot to run:"
+	printf "\n\tgit submodule update --init --recursive\n\n"
+fi
+
+# Check YouCompleteMe for running install.sh
+#if [[ ! -f "$PWD/bundle/YouCompleteMe/third_party/ycmd/build.py" ]]; then
+	#$HOME/.vim/bundle/YouCompleteMe/install.sh
+#fi
 
 if [ -e $HOME/.vimrc ]; then
 	mv $HOME/.vimrc $HOME/.vimrc.old && echo "Archiving old vimrc"
