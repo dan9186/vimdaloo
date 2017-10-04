0518296aa7bae1c82bd0e29657037a19a699cc34
Daniel Hess <dan9186@gmail.com>
2017-10-04 14:16:07 -0700

    working into a better install script

--- README.md
+++ README.md
@@ -1,23 +1,6 @@
 # Vimderp
 Easy installation and configuration of Vim to act more like a full blown version of IDEs that everyone loves.
 
-# Prerequisits
-
-## System
-
-### OS X
-```bash
-brew install vim macvim cmake ctags
-```
-
-### Linux
-```bash
-apt-get install vim cmake ctags
-```
-
-## Pathogen
-This repo depends on the use of the [pathogen](https://github.com/tpope/vim-pathogen) plugin for vim to install all the plugins.  Follow it's installation instructions before proceeding.
-
 # Installation
 Git submodules are in use with this repo.  To avoid extra confusion, the recursive flag will download all the submodules when initially checking it out.
 
--- install.sh
+++ install.sh
@@ -1,14 +1,43 @@
 #!/bin/bash
 
-# Singular deps check, may want foreach later
-DEPS=$(ls "$HOME/.vim/bundle" | head -n 1)
-if [[ ! -f "$HOME/.vim/bundle/$DEPS/.git" ]]; then
-	echo "Dependencies don't exist; you probably forgot to run:"
-	printf "\n\tgit submodule update --init --recursive\n\n"
-	exit 1
-fi
+install_prereqs() {
+	local OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 
-# Check YouCompleteMe for running install.sh
+	local install_cmd=""
+	case $OS in
+		"darwin")
+			install_cmd="brew install -y"
+			;;
+		"")
+			echo "OS Name came back blank, can't proceed"
+			exit 1
+			;;
+		*)
+			echo "${OS} is currently not supported by this install script"
+			exit 1
+			;;
+	esac
+
+	hash vim && hash mvim && hash cmake && hash ctags || ${install_cmd} vim macvim cmake ctags 2>/dev/null
+}
+
+check_git_deps() {
+	DEPS=$(ls "$HOME/.vim/bundle" | head -n 1)
+	if [[ ! -f "$HOME/.vim/bundle/$DEPS/.git" ]]; then
+		echo "Dependencies don't exist; you probably forgot to run:"
+		printf "\n\tgit submodule update --init --recursive\n\n"
+		exit 1
+	fi
+}
+
+configure() {
+	install_prereqs
+	check_git_deps
+}
+
+configure
+
+
 $HOME/.vim/bundle/YouCompleteMe/install.py --gocode-completer
 
 [ -e "$HOME/.vimrc" -a ! -h "$HOME/.vimrc" ] && mv $HOME/.vimrc $HOME/.vimrc.old && echo "Archiving old vimrc"
