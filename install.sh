784f837c300c2a4caca4a1140ed263d6b1a90456
Daniel Hess <dan9186@gmail.com>
2020-06-11 22:19:04 -0700

    changing to use all

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ configure() {
 configure
 
 
-$python_cmd $HOME/.vim/bundle/YouCompleteMe/install.py --gocode-completer --js-completer
+$python_cmd $HOME/.vim/bundle/YouCompleteMe/install.py --all
 
 [ -e "$HOME/.vimrc" -a ! -h "$HOME/.vimrc" ] && mv $HOME/.vimrc $HOME/.vimrc.old && echo "Archiving old vimrc"
 
